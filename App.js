a48e675b01d088f649d1672feac0f5d4e55ff0e0
MikeBrozhko <michael.brozhko@aventus.io>
2022-01-19 01:46:37 +0200

    Fix: Resolved issue of not rendering

--- App.js
+++ App.js
@@ -1,35 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+//Loading screen
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Loading....</Text>
+    </View>
+  );
+}
+
+//Home screen
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This will be the home screen of our application</Text>
+    </View>
+  );
+}
 
 export default function App() {
-  
-
-  //Home screen
-  const Home = () => {
-    return (
-      <View style={styles.container}>
-        <Text>This will be the home screen of our application</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
-  //Loading screen
-  const Loading = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Loading....</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
   const [fetchingData, setFetchingDataState] = React.useState(true);
   setTimeout(() => {
     setFetchingDataState(false);
   }, 1000);
-
-  if(fetchingData) {
+  
+  if (fetchingData) {
     return <Loading />
   } else {
     return <Home />
