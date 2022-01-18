60ec7f44509c5619b93867f561220c801a7a0f0b
MikeBrozhko <michael.brozhko@aventus.io>
2022-01-19 01:23:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This will be the home screen of our application</Text>
       <StatusBar style="auto" />
     </View>
   );
